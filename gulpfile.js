8f5a622b68353da555c3b49a3b9e46890ab4bc33
Domas Trijonis <domas.trijonis@gmail.com>
2016-03-06 19:01:50 +0100

    example with browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task('build-less', function () {
     return gulp.src("./less/**/*.main.less")
@@ -10,8 +12,19 @@ gulp.task('build-less', function () {
 		.pipe(rename(function(path){
 			path.basename = path.basename.replace('.main','');
 		}))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 })
+
+gulp.task('serve',['build-less'], function(){
+    browserSync.init({
+        server: "./"
+    });
+  
+    gulp.watch("./*.html").on('change', browserSync.reload);
+  
+    gulp.watch('less/**/*', ['build-less']);
+});
 
 gulp.task('watch',['build-less'], function(){
     gulp.watch('less/**/*', ['build-less']);
